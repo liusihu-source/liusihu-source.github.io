0af18ab06a3fda9f6b08dc9a5508c7c3029829c4
刘四虎 <221302071@njnu.edu.cn>
2026-04-15 17:00:48 +0800

    feat: 更新20260415

---
+++ assets/计算机_AI_常用生成.md.B_eM8_SR.js
@@ -0,0 +1,15 @@
+import{_ as s,c as n,a2 as p,o as e}from"./chunks/framework.BQmytedh.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"计算机/AI/常用生成.md","filePath":"计算机/AI/常用生成.md","lastUpdated":null}'),t={name:"计算机/AI/常用生成.md"};function l(i,a,o,c,r,d){return e(),n("div",null,a[0]||(a[0]=[p(`<h3 id="prompt-静态页" tabindex="-1">prompt - 静态页 <a class="header-anchor" href="#prompt-静态页" aria-label="Permalink to &quot;prompt - 静态页&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>请使用Figma MCP,将我选中的 Figma 设计稿**1:1 精准还原**为 Vue3 静态页面，存放到index.html中。figma链接：https://www.figma.com/design/MIQGEU1EkwUgu8ZUNZPPEr/%E7%BA%BF%E4%B8%8B%E5%9F%B9%E8%AE%AD%E5%B9%B3%E5%8F%B0?node-id=926-6042&amp;t=EJGv1Udw6i1X0jeI-4</span></span>
+<span class="line"><span>要求如下：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>1. vue3引入 /js/vue.global.js文件,使用 Vue3 + 组合式 API + &lt;script setup&gt; 语法</span></span>
+<span class="line"><span>2. 生成的css存到 /css/index.css文件中,使用原生 CSS，所有css样式名前边都用s_前缀</span></span>
+<span class="line"><span>3. 代码结构清晰、语义化 HTML、可直接运行</span></span>
+<span class="line"><span>4. 完全复刻：宽度、高度、内边距、外边距、位置、层级(z-index)</span></span>
+<span class="line"><span>5. 颜色：直接使用 Figma 取色值（十六进制 / rgba）</span></span>
+<span class="line"><span>6. 字体：字号、字重、行高、字体家族完全与设计一致</span></span>
+<span class="line"><span>7. 效果：阴影、圆角、透明度、渐变完全还原</span></span>
+<span class="line"><span>8. 布局：使用 Flex 实现与设计相同的排版结构</span></span>
+<span class="line"><span>9. 涉及的图片 svg可以留出空位，后续我自己添加</span></span>
+<span class="line"><span>10. 交互逻辑内部不处理，只添加方法名及方法名的方法说明即可，最后输出静态页面结构与样式</span></span></code></pre></div><h3 id="prompt-样式" tabindex="-1">prompt - 样式 <a class="header-anchor" href="#prompt-样式" aria-label="Permalink to &quot;prompt - 样式&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>不要修改任意样式，只帮我改格式要求如下</span></span>
+<span class="line"><span>1.不要留任何空白行</span></span>
+<span class="line"><span>2.所有样式都直接一行下，如 .s_course_card.locked .s_course_title h4 {color: #999;}，不要换行写，，直接给我最终结果</span></span></code></pre></div><h3 id="prompt-图表" tabindex="-1">prompt - 图表 <a class="header-anchor" href="#prompt-图表" aria-label="Permalink to &quot;prompt - 图表&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span></span></span></code></pre></div>`,6)]))}const u=s(t,[["render",l]]);export{m as __pageData,u as default};
